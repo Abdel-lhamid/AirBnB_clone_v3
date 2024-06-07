413e64c8731731bfc9c27dd37752e33c554e46a8
Naouwa <densali.nahla@gmail.com>
2024-06-07 21:04:07 +0100

    fixing bugs

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""API for AirBnB_clone_v3"""
+
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -17,5 +20,5 @@ def teardown_db(exception):
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""App views for AirBnB_clone_v3"""
 from api.v1.views import app_views
 from flask import jsonify
 
