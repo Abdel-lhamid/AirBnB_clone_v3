1442205f16273fd53e094a32c2061fa2444078b3
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-06-08 11:10:00 +0100

    task 7 city controller

---
+++ api/v1/views/cities.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""view for City objects that handles all default RESTFul API actions"""
+from models import storage
+from models.city import City
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+
+
+@app_views.route('/cities', methods=['GET'], strict_slashes=False)
+def get_cities():
+    """get controller all cities"""
+    return jsonify([city.to_dict() for city in storage.all(City).values()])
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+def get_city_by_id(city_id):
+    """get controller city by id"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route('/cities', methods=['POST'], strict_slashes=False)
+def post_city():
+    """Post controller create a new city"""
+    data = request.get_json(silent=True)
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    if 'city_id' not in data:
+        abort(400, 'Missing city_id')
+    if storage.get(City, data['city_id']) is None:
+        abort(404)
+    new_city = City(**data)
+    storage.new(new_city)
+    new_city.save()
+    return jsonify(new_city.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def put_city(city_id):
+    """put controller update a city"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    data = request.get_json(silent=True)
+    if data is None:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at', 'state_id']:
+            setattr(city, key, value)
+    city.save()
+    return jsonify(city.to_dict()), 200
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(city_id):
+    """delete controller delete a city"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200
