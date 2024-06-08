1129aa756dddf7214d9095b412dbca0ab0fd13a7
Naouwa <densali.nahla@gmail.com>
2024-06-08 01:38:14 +0100

    updating index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
 """App views for AirBnB_clone_v3"""
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """Route to status page"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def count():
+    """retrieves the number of each objects by type"""
+    total_count = {}
+    classes = {"Amenity": "amenities",
+               "City": "cities",
+               "Place": "places",
+               "State": "states",
+               "Review": "reviews",
+               "User": "users"}
+    for cls in classes:
+        count = storage.count(cls)
+        total_count[classes.get(cls)] = count
+    return jsonify(total_count)
