f35c18d837ff6b54399a6da96f76266bb91fb3d0
Naouwa <densali.nahla@gmail.com>
2024-06-10 18:27:48 +0100

    fixing bugs

--- models/user.py
+++ models/user.py
@@ -27,9 +27,6 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
-        if 'password' in kwargs:
-            kwargs['password'] = hashlib.md5(kwargs['password'].
-                                             encode('utf8')).hexdigest()
         super().__init__(*args, **kwargs)
 
     def __setattr__(self, name, value):
